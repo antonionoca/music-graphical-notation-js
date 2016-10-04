9f24c97470baa33dbb6aae1e260c7d575ef2a146
Antonio Noca <antonionoca@gmail.com>
2016-10-04 19:44:59 +0100

    Refactored straightLines
    Created a new module named straight_lines.js
    Added a placeholder test to straight_lines.js

--- test/range.test.js
+++ test/range.test.js
@@ -3,11 +3,11 @@ var Range = require('../app/range.js');
 var range;
 
 describe('range.js module', function() {
-    beforeAll(function() {
-        range = new Range();
-    });
+  beforeAll(function() {
+    range = new Range();
+  });
 
-    it('should calculate a fixed range', function() {
-        expect(64).toEqual(64, range.getRange());
-    });
+  it('should calculate a fixed range', function() {
+    expect(64).toEqual(64, range.getRange());
+  });
 });
