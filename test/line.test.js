4b6592c91bba0f09ff5ea8f1750b4a95e7ab5740
Antonio Noca <antonionoca@gmail.com>
2016-10-10 19:48:33 +0100

    Extracted magic numbers from line.js
    Refactored magic numbers from line.js -> javascript constants using the .get 'pattern'.
    Added one more test to line.js to check the initialization of the constants.
    Updated README.

--- test/line.test.js
+++ test/line.test.js
@@ -8,51 +8,59 @@ describe('SpLine using Perlin Noise', function() {
 	beforeAll(function() {
 		line = new Line(0,0,0,0,0,[0]);
 	});
+	
+	it('should initialize constant values', function() {
+		expect(Line.get('PADDING')).toEqual(-20);
+		expect(Line.get('LINE_WIDTH')).toEqual(3);
+		expect(Line.get('MULTIPLIER')).toEqual(0.04);
+		expect(Line.get('DIVISOR')).toEqual(11);
+		expect(Line.get('POINTS')).toEqual(511);
+	});
 
-  describe('createSplines function', function() {
+	describe('createSplines function', function() {
 
-    it('should create splines', function() {
-      var noise = [0,0,0,0];
-      var size = {x:480, y:30};
-      var ratio = 1.244;
+		it('should create splines', function() {
+			var noise = [0,0,0,0];
+			var size = {x:480, y:30};
+			var ratio = 1.244;
 
-      var splines = line.createSplines(noise, size,ratio);
-      assert.equal(4, splines.length);
-    });
+			var splines = line.createSplines(noise, size,ratio);
+			assert.equal(4, splines.length);
+		});
 
-  });
+	});
 
-  describe('update function', function() {
-    
-    it('should set verticesNeedUpdate to true', function() {
-      line.update([0]);
-      assert.equal(true, line.mesh.geometry.verticesNeedUpdate);
-    });
+	describe('update function', function() {
 
-    it('should return null when there are null vertices', function() {
-      line.geometry.vertices = null;
-      assert.equal(null, line.update());
-    });
+		it('should set verticesNeedUpdate to true', function() {
+			line.update([0]);
+			assert.equal(true, line.mesh.geometry.verticesNeedUpdate);
+		});
 
-    it('should return null when there are no vertices', function() {
-      line.geometry.vertices = []; 
-      assert.equal(null, line.update());
-    });
+		it('should return null when there are null vertices', function() {
+			line.geometry.vertices = null;
+			assert.equal(null, line.update());
+		});
 
-  });
+		it('should return null when there are no vertices', function() {
+			line.geometry.vertices = []; 
+			assert.equal(null, line.update());
+		});
 
-  describe('generatedPoints function', function() {
-  
-    it('should return null when no argument is passed', function() {
-      assert.equal(null, line.generatedPoints());
-    });
+	});
 
-    it('should return randomnized line when argument is an empty array', function() {
-      expect(line.generatedPoints([])).not.toBe(line.generatedPoints([]));
-    });
+	describe('generatedPoints function', function() {
 
-    it('should return randomnized line when argument is the same', function() {
-      expect(line.generatedPoints([0])).not.toBe(line.generatedPoints([0]));
-    });
-  });
+		it('should return null when no argument is passed', function() {
+			assert.equal(null, line.generatedPoints());
+		});
+
+		it('should return randomnized line when argument is an empty array', function() {
+			expect(line.generatedPoints([])).not.toBe(line.generatedPoints([]));
+		});
+
+		it('should return randomnized line when argument is the same', function() {
+			expect(line.generatedPoints([0])).not.toBe(line.generatedPoints([0]));
+		});
+	});
 });
