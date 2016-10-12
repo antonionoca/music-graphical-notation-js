0a0c997f65a914340140f3da5edea5b573d98c79
Antonio Noca <antonionoca@gmail.com>
2016-10-12 19:42:41 +0100

    Initial refactor on main.js
    Created animation.js and playback.js.
    Extracted some loose variables from main.js as public properties on animation.js and playback.js

--- app/main.js
+++ app/main.js
@@ -3,6 +3,12 @@
 //TODO this code is full of magic numbers
 var THREE = require('three');
 
+var Playback = require('./playback');
+var playback = new Playback();
+
+var Animation = require('./animation');
+var animation = new Animation(THREE);
+
 //--
 //TODO playback code
 //TODO Audio is a potential jshint global issue
@@ -20,31 +26,14 @@ audioAnalyser.analyser.fftSize = 2048;
 //--
 
 //TODO animation code
-var renderer, camera, scene;
 var counter = 0;
-var line = require('./line');
-var SIZE = {x: window.innerWidth / 2, y: 30};
 
-renderer = new THREE.WebGLRenderer({
-    antialias : true,
-    clearColor: 0
-});
-document.body.appendChild(renderer.domElement);
-
-//TODO scene
-camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.01, 4000 );
-camera.position.set(0, 45, 240);
-
-scene = new THREE.Scene();
+document.body.appendChild(animation.renderer.domElement);
 
 //TODO too many loose iterators and calculators
 //TODO animation code
 var lines = new Array(22);
-for (var i = lines.length - 1; i >= 0; i--) {
-    //TODO ugly object instantiation
-    lines[i] = new line(-SIZE.y/2 + i, 50, i, SIZE, Math.random() * i >> 0, audioAnalyser.frequencies());
-    scene.add(lines[i].mesh);
-}
+animation.initializeLines(lines, audioAnalyser);
 
 //TODO window
 onResize();
@@ -58,14 +47,14 @@ function update()
     for (var i = lines.length - 1; i >= 0; i--) {
         lines[i].update( audioAnalyser.frequencies() );
     }
-    renderer.render(scene, camera);
+    animation.renderer.render(animation.scene, animation.camera);
 
     //TODO jshint global issue. who owns this?
     requestAnimationFrame(update);
 }
 
 function onResize() {
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
+    animation.renderer.setSize(window.innerWidth, window.innerHeight);
+    animation.camera.aspect = window.innerWidth / window.innerHeight;
+    animation.camera.updateProjectionMatrix();
 }
---
+++ app/playback.js
@@ -0,0 +1,6 @@
+'use strict';
+
+var Playback = function() {
+};
+
+module.exports = Playback;
