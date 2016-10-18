ea688a411d02e9141a1bb99d90a015e1d23f38df
Antonio Noca <antonionoca@gmail.com>
2016-10-18 19:30:57 +0100

    Code coverage and extracted magic numbers
    Extracted magic numbers from animation, main and playback into constants.
    Improved coverage on animation.js and playback.js
    
    Next steps:
    Send mp3 file from the command line

--- app/animation.js
+++ app/animation.js
@@ -7,17 +7,56 @@ var Animation = function(THREE) {
 		antialias : true,
 		clearColor: 0
 	});
-	this.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.01, 4000 );
-	this.camera.position.set(0, 45, 240);
+
+	this.camera = new THREE.PerspectiveCamera(
+		Animation.get('FRUSTUM'), 
+		Animation.get('WINDOW_RATIO'), 
+		Animation.get('CAMERA_NEAR'), 
+		Animation.get('CAMERA_FAR')
+	);
+
+	this.camera.position.set(
+		Animation.get('CAMERA_X'), 
+		Animation.get('CAMERA_Y'), 
+		Animation.get('CAMERA_Z')
+	);
 
 	this.scene = new THREE.Scene();
-	this.SIZE = {x: window.innerWidth / 2, y: 30};
-	this.lines = new Array(22);
+	this.SIZE = {
+		x: Animation.get('SIZE_X'), 
+		y: Animation.get('SIZE_Y')
+	};
+
+	this.lines = new Array(Animation.get('NUM_LINES'));
+};
+
+Animation.get = function(name) {
+	var constants = {
+		'FRUSTUM': 45,
+		'WINDOW_RATIO': window.innerWidth / window.innerHeight,
+		'CAMERA_X': 0,
+		'CAMERA_Y': 45,
+		'CAMERA_Z': 240,
+		'CAMERA_NEAR': 0.01,
+		'CAMERA_FAR': 4000,
+		'NUM_LINES': 22,
+		'SIZE_X': window.innerWidth / 2,
+		'SIZE_Y': 30
+	};
+
+	return constants[name];
 };
 
 Animation.prototype.initializeLines = function(lines, audioAnalyser) {
 	for (var i = lines.length - 1; i >= 0; i--) {
-		lines[i] = new line(-this.SIZE.y/2 + i, 50, i, this.SIZE, Math.random() * i >> 0, audioAnalyser.frequencies());
+		lines[i] = new line(
+			-this.SIZE.y/2 + i, 
+			50, 
+			i, 
+			this.SIZE, 
+			Math.random() * i >> 0, 
+			audioAnalyser.frequencies());
+
 		this.scene.add(lines[i].mesh);
 	}
 };
@@ -34,7 +73,7 @@ Animation.prototype.updateLines = function(audioAnalyser) {
 
 Animation.prototype.onResize = function(win) {
     this.renderer.setSize(win.innerWidth, win.innerHeight);
-    this.camera.aspect = win.innerWidth / win.innerHeight;
+    this.camera.aspect = Animation.get('WINDOW_RATIO');
     this.camera.updateProjectionMatrix();
 };
 
